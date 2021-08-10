ea8153658ea2267971043a742c798bc304c26ab6
Neilphpbeginner <lemmer.neil@gmail.com>
2021-08-10 12:18:11 +0200

    Google test as per next example

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,23 @@ import "../styles/globals.css";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "../src/MaterialUI Theme/theme";
 import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider theme={theme}>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-L2VS7L8PKX"
-      ></Script>
       <Component {...pageProps} />;
     </ThemeProvider>
   );
