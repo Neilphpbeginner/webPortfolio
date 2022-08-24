a0b010d6aafeac2fcbde0f43e66f5df96481c76a
Neilphpbeginner <lemmer.neil@gmail.com>
2022-08-24 12:00:05 +0200

    Updating web profile for future developement.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-module.exports = {
-  target: "serverless",
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-      "/contact": { page: "/contact" },
-      "/projects": { page: "/projects" },
-    };
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 };
+
+module.exports = nextConfig;
